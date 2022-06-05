872fbc890efa3426537f56c4b19233293605932c
hujun <790941098@qq.com>
2022-06-05 22:58:11 +0800

    second commit

---
+++ app/src/main/java/com/androidclass/fragmenttest/News.java
@@ -0,0 +1,22 @@
+package com.androidclass.fragmenttest;
+
+public class News {
+    private String title;
+    private Sting content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Sting getContent() {
+        return content;
+    }
+
+    public void setContent(Sting content) {
+        this.content = content;
+    }
+}
